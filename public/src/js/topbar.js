70fb36dfe99d08e675c128f579e0e2cf7f9e3585
PEPEMAN <raghavwastaken1@gmail.com>
2022-09-14 10:27:32 +0530

    header

---
+++ public/src/js/topbar.js
@@ -0,0 +1,46 @@
+const topbar = document.querySelector('.topbar')
+const menuButton = document.querySelector(".topbar-inner__small-bars");
+const menuButtonContent = document.querySelector(
+    ".topbar-inner__small-bars > .fa-bars"
+);
+// console.log(menuButton);
+const menuContainer = document.querySelector(".topbar-control-container");
+
+function matchMenuIcon() {
+    if (menuContainer.classList.contains("opened")) {
+        menuButtonContent.classList.add("fa-close");
+        menuButtonContent.classList.remove("fa-bars");
+    } else {
+        menuButtonContent.classList.add("fa-bars");
+        menuButtonContent.classList.remove("fa-close");
+    }
+}
+
+function changeMenuStateSM() {
+    if (menuContainer.classList.contains("closed")) {
+        menuContainer.classList.add("opened");
+        menuContainer.classList.remove("closed");
+    } else {
+        menuContainer.classList.add("closed");
+        menuContainer.classList.remove("opened");
+    }
+
+    matchTopBarColor();
+    matchMenuIcon();
+}
+function matchTopBarColor(){
+    if (menuContainer.classList.contains("opened")){
+        topbar.style.backgroundColor="#191a1b"
+    }
+    else{
+        topbar.style.backgroundColor=""
+
+    }
+}
+function menuClickHandler() {
+    changeMenuStateSM();
+}
+menuButton.onclick = menuClickHandler
+matchMenuIcon();
+matchTopBarColor();
+
